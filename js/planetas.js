5e67425a6ec0603374311f975b33e660ed75dccd
Maria Cecília <o.cecilia@aluno.ifsp.edu>
2025-05-20 17:48:44 -0300

    teste js para carrossel

---
+++ js/planetas.js
@@ -0,0 +1,41 @@
+let planeta = [
+    {
+       nome: 'Mercúrio',
+       foto: [
+        {url: 'mercurio.png'}
+       ],
+       texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime at perferendis obcaecati, earum porro.'
+    },
+
+    {
+        nome: 'Vênus',
+        foto: [
+         {url: 'venus.png'}
+        ],
+        texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime at perferendis obcaecati, earum porro.'
+     },
+
+     {
+        nome: 'Terra',
+        foto: [
+         {url: 'terra.png'}
+        ],
+        texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime at perferendis obcaecati, earum porro.'
+     },
+
+     {
+        nome: 'Marte',
+        foto: [
+         {url: 'marte.png'}
+        ],
+        texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime at perferendis obcaecati, earum porro.'
+     },
+
+     {
+        nome: 'Júpiter',
+        foto: [
+         {url: 'jupiter.png'}
+        ],
+        texto: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime at perferendis obcaecati, earum porro.'
+     },
+]
---
+++ js/script.js
@@ -0,0 +1,1 @@
+const nomePlaneta = document.querySelector('.nome-planeta-P p')
