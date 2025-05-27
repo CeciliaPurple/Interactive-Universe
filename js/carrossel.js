0dc4a4104fdb8c4dafbfbc3395f1683ef1dbf888
deivinho007 <deivid.santana@aluno.ifsp.edu.br>
2025-05-27 18:56:32 -0300

    cagada arrumada

--- js/carrossel.js
+++ js/carrossel.js
@@ -2,49 +2,49 @@ document.addEventListener('DOMContentLoaded', function() {
     const planetas = [
         {
             nome: "Mercúrio",
-            imagem: "img/mercurio.png",
+            imagem: "../img/mercurio.png",
             descricao: "Mercúrio é pequeno, muito quente durante o dia e extremamente frio à noite,devido à falta de atmosfera.",
             link: "mercurio.html"
         },
         {
             nome: "Vênus",
-            imagem: "img/venus.png",
+            imagem: "../img/venus.png",
             descricao: "Vênus é o segundo planeta do Sistema Solar,perto da Terra, mas com condições muito diferentes.",
             link: "venus.html"
         },
         {
             nome: "Terra",
-            imagem: "img/terra.png",
+            imagem: "../img/terra.png",
             descricao: "O planeta Terra é o terceiro planeta do Sistema Solar e o único conhecido até agora que possui vida.",
             link: "terra.html"
         },
         {
             nome: "Marte",
-            imagem: "img/marte.png",
+            imagem: "../img/marte.png",
             descricao: "Marte é o quarto planeta do Sistema Solar. Conhecido como o 'planeta vermelho' devido à sua cor,causada pelo óxido de ferro.",
             link: "marte.html"
         },
         {
             nome: "Júpiter",
-            imagem: "img/jupiter.png",
+            imagem: "../img/jupiter.png",
             descricao: "Júpiter é o maior planeta do Sistema Solar. Ele é um gigante gasoso,composto principalmente de hidrogênio e hélio.",
             link: "jupiter.html"
         },
         {
             nome: "Saturno",
-            imagem: "img/saturno.png",
+            imagem: "../img/saturno.png",
             descricao: "Saturno é o sexto planeta a partir do Sol e o segundo maior do Sistema Solar, atrás apenas de Júpiter.",
             link: "saturno.html"
         },
         {
             nome: "Urano",
-            imagem: "img/urano.png",
+            imagem: "../img/urano.png",
             descricao: "Urano é o sétimo planeta a contar do Sol, um gigante gasoso azul-esverdeado, e o terceiro maior do nosso sistema solar.",
             link: "urano.html"
         },
         {
             nome: "Netuno",
-            imagem: "img/netuno.png",
+            imagem: "../img/netuno.png",
             descricao: "Netuno é o oitavo planeta do Sistema Solar,o mais distante do Sol e é um gigante gasoso devido à presença de metano.",
             link: "netuno.html"
         }
